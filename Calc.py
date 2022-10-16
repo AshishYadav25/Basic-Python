c489cc575c4618922e7012135aeefc5d575e370a
Ashish Yadav <ashish.yadav@ashish.yadav-MBP16.bbrouter>
2022-10-17 03:25:25 +0530

    Calculator precedence

---
+++ Calc.py
@@ -0,0 +1,16 @@
+print(2+3*5)
+print(4/2)
+print(2/4) #floating point division
+print(4//2) #integer division
+print(2//4) 
+print(2**3)
+print(2**0.5) #square root
+print(round(2**0.5,4)) #round of upto 4 digits
+
+print(3%2) #Modulo/Remainder
+
+print((2+3)*2) # 5x2
+print((2+3) *5/2%6) #Same presedense, associativity rule ==== 12.5%6 Left --> Right
+print(12.5%6)
+
+print(2**3**2) #Exponent associativity right to left ==== 2**9 
