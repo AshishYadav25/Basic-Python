dcea159fc9078a26ecaae95343e581c301822a61
Ashish Yadav <ashish.yadav@ashish.yadav-MBP16.bbrouter>
2022-10-17 00:36:06 +0530

    Practice 1

--- hello.py
+++ hello.py
@@ -1,1 +1,7 @@
 print ("Hello World")
+print ('I\'m Ashish')  
+print ("This is double backslash \\\\") #Comment 
+print ("line A \\n line B")
+print ("lne A \\t line B")
+print ("line A \\\\ n line B")
+print ("\\\" \\\'")
