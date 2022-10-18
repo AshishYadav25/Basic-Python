111a321fca31c20d0439f3658b0b28203246ae48
Ashish Yadav <ashish.yadav@ashish.yadav-MBP16.bbrouter>
2022-10-19 00:50:58 +0530

    formatting

--- Sting_formatting.py
+++ Sting_formatting.py
@@ -1,21 +1,21 @@
-# name = "Ashish"
-# age = 25
-# print ("Hello " + name + " your age is " + str(age))
+name = "Ashish"
+age = 25
+print ("Hello " + name + " your age is " + str(age))
 
-# print("Hello {} your age is {} ".format(name,age)) #Python3 formatting
+print("Hello {} your age is {} ".format(name,age)) #Python3 formatting
 
-# print(f"Hello {name} your age is {age}") #Python3.6 formatting --> Never forget f
-# print(f"Hello {name} your age is {age +2}") 
+print(f"Hello {name} your age is {age}") #Python3.6 formatting --> Never forget f
+print(f"Hello {name} your age is {age +2}") 
 
 #Exercise 
 #Method 1
 
-# a= int(input("number_1"))
-# b= int(input("number_2"))
-# c= int(input("number_3"))
+a= int(input("number_1"))
+b= int(input("number_2"))
+c= int(input("number_3"))
 
-# avg = int((a+b+c)/3)
-# print("Average of all number is " + str(avg))
+avg = int((a+b+c)/3)
+print("Average of all number is " + str(avg))
 
 #Method 2
 
